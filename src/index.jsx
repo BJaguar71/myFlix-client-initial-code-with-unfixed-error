27929a7045d9e349b32334421c010d3b69168c2d
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-08-15 13:12:00 +0200

    imported MainView component

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Importing the MainView component
+import { MainView } from './components/main-view/main-view';
+
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
