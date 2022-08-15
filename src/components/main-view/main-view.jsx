88be38ae6a96c1e57fbf548a4d3296b29e8c1e0e
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-08-15 12:08:38 +0200

    made and exposed the MainView component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,2 +1,15 @@
 // Importing React to create the component
 import React from  'react';
+// Making and exposing the MainView component in order to be usable in other files using the React.Component template
+export class MainView extends React.Component {
+  // Rendering the visual representation of the component
+  render () {
+    return (
+      <div className="main-view">
+        <div>Paris Is Burning</div>
+        <div>Boy I Am</div>
+        <div>There's Something In The Water</div>
+      </div>
+    );
+  }
+}
