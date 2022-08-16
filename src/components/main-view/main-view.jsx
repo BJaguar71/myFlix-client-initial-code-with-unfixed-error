ffc2f0fabeb96d1b789a94173c581a7aeec198ed
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-08-16 18:45:36 +0200

    defined the event handling to select a movie
    afterwards the information like Title, Director's name, summary of the movie, genre, year and the poster will appear

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -18,8 +18,19 @@ export class MainView extends React.Component {
         {_id: 2, Title:"Paris Is Burning", Director:{ Name:"Jennie Livingston", Bio:"Jennie Livingston (February 24, 1962) is a director and writer, known for Paris Is Burning (1990), Pose (2018) and Who's the Top? (2005). Livingston is the niece of Hollywood film director Alan J. Pakula, Step-cousin of writer-producer Jon Boorstin, actor Bob Boorstin and Anna Boorstin. She attended NYU/Tisch School of the Arts.", Image:"https://www.imdb.com/name/nm0515255/mediaviewer/rm1350167553?ref_=ext_shr_lnk"}, Summary:"A chronicle of New York's drag scene in the 1980s, focusing on balls, voguing and the ambitions and dreams of those who gave the era its warmth and vitality.", Genre:{ Name:"Documentary", Description:"A non-fictional motion-picture intended to document reality, primarily for the purposes of instruction, education or maintaining a historical record."}, Image:"https://upload.wikimedia.org/wikipedia/en/0/0d/Paris-is-burning-movie-poster-md.jpg",Year:1990.0, Featured:true},
         {_id: 3, Title:"There's Something in the Water", Director:{Name:"Elliot Page", Bio:"Elliot Page (February 21, 1987) is a Canadian actor and producer. Page has received various accolades, including an Oscar nomination, two BAFTA and Emmy nominations, and a Satellite Award. Page publicly came out as transgender in December 2020. In March 2021, he became the first openly trans man to appear on the cover of Time magazine.", Image:"https://www.imdb.com/name/nm0680983/mediaviewer/rm1295716609?ref_=ext_shr_lnk"}, Summary:"The injustices and injuries caused by environmental racism in her home province, in this urgent documentary on Indigenous and African Nova Scotian women fighting to protect their communities, their land, and their futures.", Genre:{Name:"Documenatry",Description:"A non-fictional motion-picture intended to document reality, primarily for the purposes of instruction, education or maintaining a historical record."}, Image:"https://m.media-amazon.com/images/M/MV5BN2Y2MTkxYzItZGE3Ni00MTY1LTg4YjMtM2NjNTUyOWYwZjU2XkEyXkFqcGdeQXVyNTM0NTU5Mg@@._V1_FMjpg_UX1000_.jpg", Year:2019, Featured:true}
       ],
+      selectedMovie: null
+    };
+  }
+
+  // creating setSelectedMovie component
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie
+    });
+  }
   // Rendering the visual representation of the component
   render () {
+    const { movies, selectedMovie } = this.state;
 
     if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
 
@@ -27,6 +38,11 @@ export class MainView extends React.Component {
       <div className="main-view">
         {selectedMovie
         ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+
+        : movies.map(movie => (
+          <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+        ))
+        }
       </div>
     );
   }
