76a8a4a2ebd1490b07c23e8c6716f2a031ecbc98
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-08-16 18:44:01 +0200

    updated the MovieView component to add information to be showed on screen for each movie

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,2 +1,31 @@
 import React from 'react';
 
+export class MovieView extends React.Component {
+  render() {
+    const { movie, onBackClick } = this.props;
+    return (
+      <div className="movie-view">
+        <div className="movie-poster">
+          <img src={movie.Image} />
+        </div>
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
+        <div className="genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Summary: </span>
+          <span className="value">{movie.Summary}</span>
+        </div>
+        <button onClick={() => { onBackClick(null); }}>Back</button>
+      </div>
+    );
+  }
+}
