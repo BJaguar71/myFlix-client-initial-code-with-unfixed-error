802ea8fe3f71a5eb6adffbd46b75f17314df3263
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-08-21 14:56:45 +0200

    added spacing

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -19,6 +19,7 @@ export class MovieView extends React.Component {
 
   render() {
     const { movie, onBackClick } = this.props;
+
     return (
       <div className="movie-view">
         <div className="movie-poster">
