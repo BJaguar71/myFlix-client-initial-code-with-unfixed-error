6cf0553499ac8b2d47d22543a61852f8aa7ea5ee
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-08-21 14:57:49 +0200

    defined the Proptypes for movieView

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -60,3 +60,24 @@ export class MovieView extends React.Component {
     );
   }
 }
+
+MovieView.PropTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string
+    }),
+    Summary: PropTypes.string.isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Image: PropTypes.string.isRequired
+    }),
+    Actors: PropTypes.array.isRequired,
+    Image: PropTypes.string.isRequired,
+    Year: PropTypes.number.isRequired,
+    Featured: PropTypes.bool
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
