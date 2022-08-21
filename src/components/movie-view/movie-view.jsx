4e35683bf0727cdb32cc78c5f14275b86abad26a
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-08-21 14:56:36 +0200

    defined a component will unmount inside th component's lifecycle to remove the eventListener

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -13,6 +13,10 @@ export class MovieView extends React.Component {
     document.addEventListener('keypress', this.keypressCallback)
   }
 
+  componentWillUnmount() {
+    document.removeEventListener('keypress', this.keypressCallback)
+  }
+
   render() {
     const { movie, onBackClick } = this.props;
     return (
