9105b02edd534ca6e6ba6ba749d9e653b634b719
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-08-21 14:52:38 +0200

    imported the movie-card stylesheet

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 // importing PropTyes
 import PropTypes from 'prop-types';
 
+import './movie-card.scss';
+
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
