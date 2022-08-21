0248cfb974a2032eb20256f54fe9b7450bfe4800
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-08-21 14:38:03 +0200

    created the LoginView component using react function component

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -4,3 +4,24 @@ import PropTypes from 'prop-types';
 
 import './login-view.scss';
 
+export function LoginView(props) {
+  // called useState() with empty string, the intial value of login variable
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+  // returns an array of paired values, destructured above
+
+
+  return(
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Login</button>
+    </form>
+  );
+}
