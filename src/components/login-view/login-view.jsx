fe4c4dd2216021fbcc04a1dce76de5a20960ad82
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-08-22 14:36:31 +0200

    updated login-view defined prop-types

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -34,9 +34,5 @@ export function LoginView(props) {
 }
 
 LoginView.propTypes = {
-  user: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-    password: PropTypes.string.isRequired
-  }).isRequired,
   onLoggedIn: PropTypes.func.isRequired
 };
