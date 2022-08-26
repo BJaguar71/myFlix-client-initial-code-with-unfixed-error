aa7f6940dc31f0ac950c5d7a7e45ce0e60faeac3
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-08-26 15:57:55 +0200

    updated the handleSubmit to be able to send requests to the server for authentication

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -17,10 +17,18 @@ export function LoginView(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-    props.onLoggedIn(username);
+    // send a request to the server for authentication
+    axios.post('https://t-flix.herokuapp.com/login', {
+      Username: username,
+      Password: password
+    })
+    .then(response => {
+      const data = response.data;
+      props.onLoggedIn(data);
+    })
+    .catch(e => {
+      console.log('no such user')
+    });
   };
 
   return(
