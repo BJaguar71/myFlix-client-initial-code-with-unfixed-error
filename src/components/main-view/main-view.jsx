dbf10f9a51882d978d13cb5a2710e981a0a5b417
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-08-28 15:36:57 +0200

    added space

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -71,6 +71,7 @@ export class MainView extends React.Component {
     this.setState({
       user: authData.user.Username
     });
+
     localStorage.setItem('token', authData.token);
     localStorage.setItem('user', authData.user.Username);
     this.getMovies(authData.token);
