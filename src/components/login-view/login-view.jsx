c6ed294a1eba1fd0ef09ce9852396503ebb30b4f
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-08-28 15:34:03 +0200

    added code to display validation error

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -60,11 +60,15 @@ export function LoginView(props) {
       <Form.Group controlId="formUsername">
         <Form.Label>Username:</Form.Label>
         <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+        {/* code added here to display validation error */}
+        {username && <p>{usernameErr}</p>}
       </Form.Group>
 
       <Form.Group controlId="formPassword">
         <Form.Label>Password:</Form.Label>
         <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+        {/* code added here to display validation error */}
+        {passwordErr && <p>{passwordErr}</p>}
       </Form.Group>
       <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
     </Form>
