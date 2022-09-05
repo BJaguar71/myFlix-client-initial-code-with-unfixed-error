de988176917821cb204956f8444d6929ccb6a139
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-09-05 18:07:58 +0200

    created route to genreView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -117,6 +117,12 @@ export class MainView extends React.Component {
               <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director} onBackClick={() => history.goBack()} />
             </Col>
           }} />
+          <Route exact path="/genre/:name" render={({ match, history }) => {
+            if (movies.length === 0 ) return <div className="main-view" />;
+            return <Col md={8}>
+              <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre} onBackClick={() => history.goBack()} />
+            </Col>
+          }} />
         </Row>
       </div>
   );
