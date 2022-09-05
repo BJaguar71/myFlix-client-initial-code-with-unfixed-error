997576ee03df7d183f52aafa6824bca148dbcc58
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-09-05 17:49:41 +0200

    imported link from react router dom

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card, CardGroup, Container, Col, Row, Form } from 'react-bootstrap';
-
+import { Link } from 'react-router-dom';
 import './movie-view.scss';
 
 export class MovieView extends React.Component {
