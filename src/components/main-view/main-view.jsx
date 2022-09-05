f5b2bf132f75c7eeb656ac9721de4b49d1eb6975
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-09-05 18:07:42 +0200

    created route to directorView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -111,6 +111,12 @@ export class MainView extends React.Component {
               <MovieView movie={movies.find(m => m._id === match.params.movieId)}  onBackClick={() => history.goBack()} />
             </Col>
           }} />
+          <Route exact path="/directors/:name" render={({ match, history }) => {
+            if (movies.length === 0 ) return <div className="main-view" />;
+            return <Col md={8}>
+              <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director} onBackClick={() => history.goBack()} />
+            </Col>
+          }} />
         </Row>
       </div>
   );
