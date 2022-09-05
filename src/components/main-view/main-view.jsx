63ffbbc26e12f22a4fab1e8ab477be1d638052e8
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-09-05 18:02:56 +0200

    commented previous on logged out function

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -50,14 +50,14 @@ export class MainView extends React.Component {
     this.getMovies(authData.token);
   }
 
-  // logout function
-  onLoggedOut() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-    this.setState({
-      user: null
-    });
-  }
+  // // logout function
+  // onLoggedOut() {
+  //   localStorage.removeItem('token');
+  //   localStorage.removeItem('user');
+  //   this.setState({
+  //     user: null
+  //   });
+  // }
 
   // adding a new GetMovie method in order to be able to make authenticated requests to the API and see the list of movies
   getMovies(token) {
