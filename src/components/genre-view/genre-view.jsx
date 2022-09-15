efb283408480892d17fb9e52fbb1ad5438a6ffa1
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-09-15 11:50:43 +0200

    created GenreView component

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Container, Row, Button } from 'react-bootstrap';
-import { MovieCard } from "../movie-card/movie-card";
+import { MovieCard } from '../movie-card/movie-card';
+
 
 export function GenreView(props) {
   const { genre, genreMovies, goBack } = this.props;
 
   return (
     <Container>
-      <Button variant='outline-dark' onClick={goBack}>Back</Button>
+      <Button variant="outline-dark" onClick={goBack}>
+        Back
+      </Button>
       <h1>{genre.Name}</h1>
       <h2>Description:</h2>
       <p>{genre.Description}</p>
       <h2>Movies on this genre:</h2>
-      <Row className='justify-content-center'>
+      <Row className="justify-content-center">
         {genreMovies.map((movie) => (
           <MovieCard key={movie._id} movie={movie}>
             {movie.Title}
@@ -22,3 +26,11 @@ export function GenreView(props) {
     </Container>
   );
 }
+
+GenreView.prototypes = {
+  Genre: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired
+};
