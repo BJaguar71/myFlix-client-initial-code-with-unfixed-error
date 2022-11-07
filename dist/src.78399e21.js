79996dc82821949bec5a1582ac085b5266b27864
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-11-07 14:27:21 +0100

    parcel bundled files

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -66911,7 +66911,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7072" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7289" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
